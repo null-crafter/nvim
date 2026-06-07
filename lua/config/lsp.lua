c9f78b0bd8bb05bea9eac4271688e4e96ea7d0f9
null-crafter <163591881+null-crafter@users.noreply.github.com>
2026-06-07 11:48:16 +0000

    add rust support

--- lua/config/lsp.lua
+++ lua/config/lsp.lua
@@ -1,68 +1,86 @@
 local servers = {
-  js_and_ts = {
-      cmd = {'typescript-language-server', '--stdio'},
-      filetypes = {'typescriptreact', 'typescript', 'javascriptreact', 'javascript', }, -- tsx, ts, js, jsx
-      root_markers = {'package.json', 'node_modules'},
-      settings = {},
-  },
-  expert = {
-    cmd = { 'expert', '--stdio'},
-    filetypes = { 'elixir' },
-    root_markers = {'mix.exs'},
-    settings = {
-    },
-  },
-  pyright = {
-    cmd = { 'basedpyright-langserver', '--stdio' },
-    filetypes = { 'python' },
-    root_markers = { 'pyproject.toml', 'setup.py', 'setup.cfg', 'requirements.txt', 'Pipfile', '.git' },
-    settings = {
-      python = {
-        analysis = {
-          autoSearchPaths = true,
-          useLibraryCodeForTypes = true,
-          diagnosticMode = 'openFilesOnly',
-          typeCheckingMode = 'basic',
+    rust_analyzer = {
+        cmd = { 'rust-analyzer' },
+        filetypes = { 'rust' },
+        root_markers = { 'Cargo.toml', 'Cargo.lock', '.git' },
+
+        settings = {
+            ['rust-analyzer'] = {
+                cargo = {
+                    targetDir = true,
+                    allFeatures = true,
+                    buildScripts = { enable = true },
+                },
+                procMacro = { enable = true },
+                diagnostics = { enable = true },
+
+            },
         },
-      },
     },
-  },
-  jedi = {
-    cmd = { 'jedi-language-server'},
-    filetypes = { 'python' },
-    root_markers = { 'pyproject.toml', 'setup.py', 'setup.cfg', 'requirements.txt', 'Pipfile', '.git' },
-    settings = {
+    js_and_ts = {
+        cmd = {'typescript-language-server', '--stdio'},
+        filetypes = {'typescriptreact', 'typescript', 'javascriptreact', 'javascript', }, -- tsx, ts, js, jsx
+        root_markers = {'package.json', 'node_modules'},
+        settings = {},
     },
-  },
+    expert = {
+        cmd = { 'expert', '--stdio'},
+        filetypes = { 'elixir' },
+        root_markers = {'mix.exs'},
+        settings = {
+        },
+    },
+    pyright = {
+        cmd = { 'basedpyright-langserver', '--stdio' },
+        filetypes = { 'python' },
+        root_markers = { 'pyproject.toml', 'setup.py', 'setup.cfg', 'requirements.txt', 'Pipfile', '.git' },
+        settings = {
+            python = {
+                analysis = {
+                    autoSearchPaths = true,
+                    useLibraryCodeForTypes = true,
+                    diagnosticMode = 'openFilesOnly',
+                    typeCheckingMode = 'basic',
+                },
+            },
+        },
+    },
+    jedi = {
+        cmd = { 'jedi-language-server'},
+        filetypes = { 'python' },
+        root_markers = { 'pyproject.toml', 'setup.py', 'setup.cfg', 'requirements.txt', 'Pipfile', '.git' },
+        settings = {
+        },
+    },
 
 }
 
 
 for name, cfg in pairs(servers) do
-  vim.lsp.config(name, cfg)
+    vim.lsp.config(name, cfg)
 end
 vim.lsp.enable(vim.tbl_keys(servers))
 
 vim.diagnostic.config({
-  virtual_text = true,
-  signs = true,
-  underline = true,
-  update_in_insert = false,
-  severity_sort = true,
+    virtual_text = true,
+    signs = true,
+    underline = true,
+    update_in_insert = false,
+    severity_sort = true,
 })
 
 vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(ev)
-    local client = vim.lsp.get_client_by_id(ev.data.client_id)
-    if not client then return end
+    callback = function(ev)
+        local client = vim.lsp.get_client_by_id(ev.data.client_id)
+        if not client then return end
 
-    if client:supports_method('textDocument/formatting') then
-      vim.api.nvim_create_autocmd('BufWritePre', {
-        buffer = ev.buf,
-        callback = function()
-          vim.lsp.buf.format({ bufnr = ev.buf, id = client.id, timeout_ms = 2000 })
-        end,
-      })
-    end
-  end,
+        if client:supports_method('textDocument/formatting') then
+            vim.api.nvim_create_autocmd('BufWritePre', {
+                buffer = ev.buf,
+                callback = function()
+                    vim.lsp.buf.format({ bufnr = ev.buf, id = client.id, timeout_ms = 2000 })
+                end,
+            })
+        end
+    end,
 })
